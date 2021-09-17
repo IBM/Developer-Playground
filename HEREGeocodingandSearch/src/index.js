9f84bc71ffe63b63481f0a908cc61512f057ae13
aishwaryapradeep01 <87689930+aishwaryapradeep01@users.noreply.github.com>
2021-09-17 17:51:28 +0530

    Delete HEREGeocodingandSearch directory

--- HEREGeocodingandSearch/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-
-import "./carbon.css";
-import "./style.css";
-
-ReactDOM.render(<App />, document.getElementById("root"));

e043b73a0a90358e33ebcc08e1a8dc5f2d7e706d
Suyash Gupte <gupte.suyash11@gmail.com>
2021-10-04 16:52:54 +0530

    added manual proxy
    proxy without using the disable_host_check flag

---
+++ src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    ['/sampledata', '/upload', '/detect', '/result?:jobid', '/downlod?:jobid'],
+    createProxyMiddleware({
+      target: 'http://localhost:4000',
+      changeOrigin: true,
+    })
+  );
+};

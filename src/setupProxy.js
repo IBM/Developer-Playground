c29c34954d242c21035961bfa20892e075280d52
Suyash Gupte <gupte.suyash11@gmail.com>
2021-10-12 16:35:05 +0530

    redirect for download

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    ['/sampledata', '/upload', '/detect', '/result?:jobid', '/status?:jobid', '/downlod?:jobid'],
+    ['/sampledata', '/upload', '/detect', '/result?:jobid', '/status?:jobid', "/gotodownlod?:jobid",'/downlod?:jobid'],
     createProxyMiddleware({
       target: 'http://localhost:4000',
       changeOrigin: true,

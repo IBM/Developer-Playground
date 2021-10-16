e52a15b495b2cea531f36fc2e16d31737f6e2f62
Suyash Gupte <gupte.suyash11@gmail.com>
2021-10-16 13:08:16 +0530

    added pup test env

--- anomaly/checkConnection.js
+++ anomaly/checkConnection.js
@@ -1,9 +1,14 @@
 const {clientId, clientSecret} = require("./credentials.js")
 const promiseRequest = require("./promiseRequest")
+let apiEndpoint;
+if(process.env.PLAYGROUND_ENVIRONMENT === 'production') 
+  apiEndpoint = 'https://api.ibm.com/ai4industry/run/connection-check'
+else
+  apiEndpoint = `https://dev.api.ibm.com/ai4industry/test/connection-check`
 
 const options = {
   method: 'GET',
-  url: 'https://api.ibm.com/ai4industry/run/connection-check',
+  url: apiEndpoint,
   headers: {
     'X-IBM-Client-Id': clientId,
     'X-IBM-Client-Secret': clientSecret,

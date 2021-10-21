ebe840f0c35e3ec7fb582bbd1e37e12513f45c02
Suyash Gupte <gupte.suyash11@gmail.com>
2021-10-21 11:25:20 +0530

    removed env check

--- anomaly/getStatus.js
+++ anomaly/getStatus.js
@@ -3,11 +3,7 @@ const storeResult = require("./storeResult")
 
 const getStatus = async (jobId) => {
     return new Promise(async function (resolve, reject) {
-        let apiEndpoint;
-        if (process.env.PLAYGROUND_ENVIRONMENT.trim() !== 'production')
-            apiEndpoint = `https://dev.api.ibm.com/ai4industry/test/result/${jobId}`
-        else
-            apiEndpoint = `https://api.ibm.com/ai4industry/run/result/${jobId}`
+        let apiEndpoint = `https://api.ibm.com/ai4industry/run/result/${jobId}`
 
         const options = {
             method: 'GET',

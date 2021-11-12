4188f4946dd476760f0165b87ee2065016f81f8c
Nupur Negi <nupurnegi@Nupurs-MacBook-Pro.local>
2021-11-12 10:22:40 +0530

    empty branch

--- client/src/assets/env.js
+++
@@ -1,9 +0,0 @@
-(function(window) {
-  window["env"] = window["env"] || {};
-
-  // Environment variables
-  window["env"]["APP_CONFIG_KEY"] = "APP_CONFIG";
-  window["env"]["API_BASE_URL"] = "http://localhost:3000/api";
-  window["env"]["IO_BASE_URL"] = "http://localhost:3000";
-  window["env"]["debug"] = true;
-})(this);

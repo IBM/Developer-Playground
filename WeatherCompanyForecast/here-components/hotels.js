1efe7f31891ad365e4d7686dc0e7c7cee7eb514b
aishwaryapradeep01 <87689930+aishwaryapradeep01@users.noreply.github.com>
2022-01-25 18:22:33 +0530

    Update hotels.js

--- WeatherCompanyForecast/here-components/hotels.js
+++ WeatherCompanyForecast/here-components/hotels.js
@@ -12,8 +12,6 @@ function HotelSearch({maplat, maplong}) {
   const [isLoading,setLoading] = useState(false);
   const [lterrstate, setlterrstate] = useState(false);
   const [lngerrstate, setlngerrstate] = useState(false);
-  // const [arerrstate, setarerrstate] = useState(false);
-  // const [arerrtext, setarerrtext] = useState('A valid value is required');
   const [lterrtext, setlterrtext] = useState('A valid value is required');
   const [lnerrtext, setlnerrtext] = useState('A valid value is required');
   const [buttonstate, setbuttonstate] = useState(false);
@@ -22,7 +20,7 @@ function HotelSearch({maplat, maplong}) {
   const [err2status, seterr2status] = useState(false);
   const [err3status, seterr3status] = useState(false);
 
-      useEffect(() => {
+  useEffect(() => {
 
     function autofill() {
       setlatitude(maplat);
@@ -96,19 +94,6 @@ const headers = [
   },
 ];
 
-// const validArea = (e) => {
-
-//       if(!e.target.value.match(/^[a-zA-Z]+$/)){
-//               setarerrstate(true)
-//               setarerrtext("Only letters allowed. No special characters or numbers");
-//               setbuttonstate(true)
-//            }
-//       else{
-//             setarerrstate(false)
-//             setbuttonstate(false)
-//         }        
-//         setareaquery(e.target.value);
-// }
 
 const validLatf = (e) => {
 
@@ -166,12 +151,8 @@ const validLongf = (e) => {
     <div className="TextArea" > 
       <TextInput id={'longitude'} labelText = {'Longitude*'} size = 'lg' invalid = {lngerrstate} invalidText = {lnerrtext}  helperText="Add valid co-ordinates." placeholder = {'Longitude'} value={longitude} onChange={validLongf} /><br/>
     </div>
-    {/* <div className="TextArea" >  
-      <TextInput id={'area-query'} labelText = {'Area*'} size = 'lg' invalid = {arerrstate} invalidText = {arerrtext} helperText="Input values only in alphabets - Public places/Hotels/Restaurants." placeholder = {'Query: Area/Address'} value={areaquery} onChange={validArea} />
-    </div>   */}
-      <br/>
       <div className="CButtonArea">
-        <Button type="submit" disabled = {buttonstate} > Get Hotels Near You </Button>
+        <Button type="submit" disabled = {buttonstate} > Get Hotels </Button>
       </div>
 
       </Form>

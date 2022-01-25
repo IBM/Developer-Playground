97570c4f0cabc054385989e3fd031dbe99ddc787
Nupur Negi <nupurn13@gmail.com>
2022-01-26 00:57:12 +0530

    reuse services & auto space creation

--- deleteservice.sh
+++ deleteservice.sh
@@ -1,22 +1,3 @@
-
-servicename="cp-cos"
-# ibmcloud resource service-key-delete "$servicename-creds" -f
-ibmcloud resource service-instance-delete $servicename -f --recursive
-# rm cos.json
-# rm cp-cos.txt
-
-servicename="cp-wml"
-# ibmcloud resource service-key-delete "$servicename-creds" -f
-ibmcloud resource service-instance-delete $servicename -f
-# rm watsonml.json
-# rm cp-wml.txt
-
-# servicename="cp-wstudio"
-# ibmcloud resource service-key-delete "$servicename-creds" -f
-# ibmcloud resource service-instance-delete $servicename -f
-# rm watsonstudio.json
-# rm cp-wstudio.txt
-
 python3.8 DeleteDeployment.py
 
 #delete the API key

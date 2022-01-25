0bbdba70b2e6703be747f046476cc58cdf41c4ad
Suyash Gupte <gupte.suyash11@gmail.com>
2022-01-25 20:25:47 +0530

    bug fix

--- delete_services.py
+++ delete_services.py
@@ -1,24 +1,23 @@
 import os
-import sys
 from dotenv import dotenv_values
 
 
 config = dotenv_values(".env") 
 updated_cos = config["CLOUD-OBJECT-STORAGE_UPDATED"]
 updated_wml = config["PM-20_UPDATED"]
-updated_wa = config["COVERSATION_UPDATED"]
+updated_wa = config["CONVERSATION_UPDATED"]
 cos_name = config["CLOUD-OBJECT-STORAGE_NAME"]
 wml_name = config["PM-20_NAME"]
 wa_name = config["CONVERSATION_NAME"]
 
-if(updated_cos):
+if(updated_cos=="True"):
     data = os.popen("ibmcloud resource service-instance-delete "+cos_name+" -f --recursive").read()
     print(data)
 
-if(updated_wml):
+if(updated_wml=="True"):
     data = os.popen("ibmcloud resource service-instance-delete "+wml_name+" -f --recursive").read()
     print(data)
 
-if(updated_wa):
+if(updated_wa=="True"):
     data = os.popen("ibmcloud resource service-instance-delete "+wa_name+" -f --recursive").read()
     print(data)

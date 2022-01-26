f2104cc45214509d554261a38c4dd29c6413c4c5
Nupur Negi <nupurn13@gmail.com>
2022-01-26 15:00:00 +0530

    delete apikey fix

--- deleteservice.sh
+++ deleteservice.sh
@@ -1,6 +1,9 @@
 python3.8 DeleteDeployment.py
 python3.8 delete_services.py
 
+# delete APIKEY using it's id to avoid incorrect APIkey deletion 
+new_filename=$(echo "$a" | sed '2!d' key_file )
+id=$(echo "$a" | cut -c 9-51 <<< $new_filename)
 #delete the API key
-ibmcloud iam api-key-delete ApiKey-bankLoan -f
+ibmcloud iam api-key-delete $id -f
 rm ${CHE_PROJECTS_ROOT}/bank-loan/key_file

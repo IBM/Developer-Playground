54fe8056a870b4f7d70dceedbede2d155594272c
Nupur Negi <nupurn13@gmail.com>
2022-01-29 11:00:48 +0530

    small fixes

--- DeployModel/DeploySavedModel.py
+++ DeployModel/DeploySavedModel.py
@@ -5,33 +5,16 @@ from dotenv import dotenv_values
 
 #bring model id and deployment space name from .env file
 config = dotenv_values(".env") 
-deployment_space_name=config["DEPLOYMENT_SPACE_NAME"]
 apikey=config["API_KEY"]
 model_name = config["MODEL_NAME"]
 loc = config["PM-20_LOC"]
-
+space_id=config["SPACE_ID"]
 #get connected to watson ML
 wml_credentials = {
   "apikey": apikey,
   "url": "https://"+loc+".ml.cloud.ibm.com"
 }
 client = APIClient(wml_credentials)
-
-MODEL_NAME = model_name
-DEPLOYMENT_SPACE_NAME = deployment_space_name
-
-#pick up the space id using the deployment space name
-client.spaces.list()
-all_spaces = client.spaces.get_details()['resources']
-space_id = None
-for space in all_spaces:
-    if space['entity']['name'] == DEPLOYMENT_SPACE_NAME:
-        space_id = space["metadata"]["id"]
-        print("\nDeployment Space GUID: ", space_id)
-
-if space_id is None:
-    print("WARNING: Your space does not exist. Create a deployment space before proceeding to the next cell.")
-
 client.set.default_space(space_id)
 asset_details = client.repository.get_details()
 for resource in asset_details["models"]["resources"] :
@@ -42,7 +25,7 @@ config = dotenv_values(".env")
 published_model_id=config["MODEL_ID"]
 #deployment of the model
 deploy_meta = {
-     client.deployments.ConfigurationMetaNames.NAME: 'Deployment of '+ MODEL_NAME,
+     client.deployments.ConfigurationMetaNames.NAME: 'Deployment of '+ model_name,
      client.deployments.ConfigurationMetaNames.ONLINE: {}
 }
 created_deployment = client.deployments.create(published_model_id, meta_props=deploy_meta)
@@ -56,10 +39,6 @@ date=now.strftime("%Y")+"-"+now.strftime("%m")+"-"+now.strftime("%d")
 
 modelurl = scoring_endpoint+"?version="+date
 
-with open("./scripts/add_model_url.sh", "r") as f :
-  filedata = f.read()
-
-filedata = filedata.replace('ADD_YOUR_MODEL_URL', modelurl)
-
-with open('./scripts/add_model_url.sh', 'w') as f:
-  f.write(filedata)
+#add the model URL to .env file
+with open(".env", "a") as f:
+    f.write("\n#MODEL URL\nMODEL_URL=\""+modelurl+"\"\n")

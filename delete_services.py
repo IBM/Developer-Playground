7e9c0ca0a32383cb2839882efb0ac355947fd793
Suyash Gupte <gupte.suyash11@gmail.com>
2022-02-02 18:22:41 +0530

    delete bux fig and print region

--- delete_services.py
+++ delete_services.py
@@ -7,23 +7,23 @@ updated_cos = config["CLOUD-OBJECT-STORAGE_UPDATED"]
 updated_wml = config["PM-20_UPDATED"]
 updated_wa = config["CONVERSATION_UPDATED"]
 updated_ws = config["DATA-SCIENCE-EXPERIENCE_UPDATED"]
-cos_name = config["CLOUD-OBJECT-STORAGE_NAME"]
-wml_name = config["PM-20_NAME"]
-wa_name = config["CONVERSATION_NAME"]
-ws_name = config["DATA-SCIENCE-EXPERIENCE_NAME"]
+cos_crn = config["CLOUD-OBJECT-STORAGE_CRN"]
+wml_crn = config["PM-20_CRN"]
+wa_crn = config["CONVERSATION_CRN"]
+ws_crn = config["DATA-SCIENCE-EXPERIENCE_CRN"]
 
 if(updated_cos=="True"):
-    data = os.popen("ibmcloud resource service-instance-delete "+cos_name+" -f --recursive").read()
+    data = os.popen("ibmcloud resource service-instance-delete "+cos_crn+" -f --recursive").read()
     print(data)
 
 if(updated_wml=="True"):
-    data = os.popen("ibmcloud resource service-instance-delete "+wml_name+" -f --recursive").read()
+    data = os.popen("ibmcloud resource service-instance-delete "+wml_crn+" -f --recursive").read()
     print(data)
 
 if(updated_wa=="True"):
-    data = os.popen("ibmcloud resource service-instance-delete "+wa_name+" -f --recursive").read()
+    data = os.popen("ibmcloud resource service-instance-delete "+wa_crn+" -f --recursive").read()
     print(data)
 
 if(updated_ws=="True"):
-    data = os.popen("ibmcloud resource service-instance-delete "+ws_name+" -f --recursive").read()
+    data = os.popen("ibmcloud resource service-instance-delete "+ws_crn+" -f --recursive").read()
     print(data)

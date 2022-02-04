bbfcdd3cf8c1e84982a849292b1adcf27d5f238a
Nupur Negi <nupurn13@gmail.com>
2022-02-04 19:30:51 +0530

    watson studio required

--- create-ibm-cloud-services.sh
+++ create-ibm-cloud-services.sh
@@ -9,3 +9,9 @@ service="pm-20"
 region="us-south"
 plan="lite"
 python3.8 gather_service_info.py $service $servicename $region $plan || exit 1
+
+servicename="cp-wstudio"
+service="data-science-experience"
+region="us-south"
+plan="free-v1"
+python3.8 gather_service_info.py $service $servicename $region $plan || exit 1

e06ca7c61e93aff25c5b63a64beacf8f6c3a7d77
Suyash Gupte <gupte.suyash11@gmail.com>
2022-02-04 19:35:05 +0530

    reuse services

--- create-ibm-cloud-services.sh
+++ create-ibm-cloud-services.sh
@@ -1,29 +1,23 @@
 servicename="cp-stt"
 service="speech-to-text"
-region="eu-gb"
-ibmcloud resource service-instance-create $servicename $service lite $region
-ibmcloud resource service-key-create "$servicename-creds" Manager --instance-name $servicename > "$servicename.txt"  2>&1
-apikey=$(cat $servicename.txt | awk '$1 == "apikey:" {print $2}')
-url=$(cat $servicename.txt | awk '$1 == "url:" {print $2}')
-JSON_STRING='{"apikey":"'"$apikey"'","url":"'"$url"'"}'
-echo $JSON_STRING > speechtotext.json
+region="us-south"
+plan="lite"
+filename="speechtotext.json"
+python3.8 gather_service_info.py $service $servicename $region $plan $filename|| exit 1
+
 
 servicename="cp-nlu"
 service="natural-language-understanding"
-region="eu-gb"
-ibmcloud resource service-instance-create $servicename $service free $region
-ibmcloud resource service-key-create "$servicename-creds" Manager --instance-name $servicename > "$servicename.txt"  2>&1
-apikey=$(cat $servicename.txt | awk '$1 == "apikey:" {print $2}')
-url=$(cat $servicename.txt | awk '$1 == "url:" {print $2}')
-JSON_STRING='{"apikey":"'"$apikey"'","url":"'"$url"'"}'
-echo $JSON_STRING > naturallanguageunderstanding.json
+region="us-south"
+plan="free"
+filename="naturallanguageunderstanding.json"
+python3.8 gather_service_info.py $service $servicename $region $plan $filename|| exit 1
+
 
 servicename="cp-ta"
 service="tone-analyzer"
-region="eu-gb"
-ibmcloud resource service-instance-create $servicename $service lite $region
-ibmcloud resource service-key-create "$servicename-creds" Manager --instance-name $servicename > "$servicename.txt"  2>&1
-apikey=$(cat $servicename.txt | awk '$1 == "apikey:" {print $2}')
-url=$(cat $servicename.txt | awk '$1 == "url:" {print $2}')
-JSON_STRING='{"apikey":"'"$apikey"'","url":"'"$url"'"}'
-echo $JSON_STRING > toneanalyzer.json
+region="us-south"
+plan="lite"
+filename="toneanalyzer.json"
+python3.8 gather_service_info.py $service $servicename $region $plan $filename|| exit 1
+

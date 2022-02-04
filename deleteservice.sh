be52815f3dd23051a47c00a3b1fd40fe19f4aecd
Suyash Gupte <gupte.suyash11@gmail.com>
2022-02-04 18:45:15 +0530

    updated code pattern

--- deleteservice.sh
+++ deleteservice.sh
@@ -1,14 +1,20 @@
-filename="speechtotext.json"
-python3.8 deleteservices.py $filename
+servicename="cp-stt"
+ibmcloud resource service-key-delete "$servicename-creds" -f
+ibmcloud resource service-instance-delete $servicename -f
 rm speechtotext.json
+rm cp-stt.txt
 touch speechtotext.json
 
-filename="naturallanguageunderstanding.json"
-python3.8 deleteservices.py $filename
+servicename="cp-nlu"
+ibmcloud resource service-key-delete "$servicename-creds" -f
+ibmcloud resource service-instance-delete $servicename -f
 rm naturallanguageunderstanding.json
+rm cp-nlu.txt
 touch naturallanguageunderstanding.json
 
-filename="toneanalyzer.json"
-python3.8 deleteservices.py $filename
+servicename="cp-ta"
+ibmcloud resource service-key-delete "$servicename-creds" -f
+ibmcloud resource service-instance-delete $servicename -f
 rm toneanalyzer.json
+rm cp-ta.txt
 touch toneanalyzer.json

259020519283fc205001d2e75f3664a22f866c39
aishwaryapradeep01 <87689930+aishwaryapradeep01@users.noreply.github.com>
2022-02-04 13:50:06 +0530

    Create key.js

---
+++ WeatherDataAssistant/key.js
@@ -0,0 +1,13 @@
+var fs = require('fs');
+var array = fs.readFileSync('key.text').toString().split("\n");
+
+function getRandomItem(arr) {
+
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    const item = arr[randomIndex];
+
+    return item;
+}
+
+const result = getRandomItem(array);
+console.log(result);

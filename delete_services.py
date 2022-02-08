fa70cdf2d9ddfc2a9f12c21d6f487565da12e64d
Nupur Negi <35726589+nupurnegi@users.noreply.github.com>
2022-02-08 18:45:17 +0530

    Update delete_services.py

--- delete_services.py
+++ delete_services.py
@@ -1,5 +1,7 @@
+
 import os
 from dotenv import dotenv_values
+import json
 
 
 config = dotenv_values(".env") 
@@ -7,11 +9,36 @@ updated_cos = config["CLOUD-OBJECT-STORAGE_UPDATED"]
 updated_wml = config["PM-20_UPDATED"]
 cos_crn = config["CLOUD-OBJECT-STORAGE_CRN"]
 wml_crn = config["PM-20_CRN"]
+cos_service_key = config["CLOUD-OBJECT-STORAGE_SERVICE_KEY"]
+# wml_service_key = config["PM-20_SERVICE_KEY"]
 
 if(updated_cos=="True"):
+    data = os.popen("ibmcloud resource service-key-delete "+cos_service_key+" -f").read()
     data = os.popen("ibmcloud resource service-instance-delete "+cos_crn+" -f --recursive").read()
+    print(data)
+
+if(updated_cos=="False" and cos_service_key!=""):
+    data = os.popen("ibmcloud resource service-key-delete "+cos_service_key+" -f").read()
     print(data)
 
 if(updated_wml=="True"):
     data = os.popen("ibmcloud resource service-instance-delete "+wml_crn+" -f --recursive").read()
     print(data)
+# if(wml_service_key!=""):
+#     data = os.popen("ibmcloud resource service-key-delete "+wml_service_key+" -f").read()
+#     print(data)
+
+# if(ws_service_key!=""):
+#     data = os.popen("ibmcloud resource service-key-delete "+ws_service_key+" -f").read()
+#     print(data)
+# try:
+#     data = os.popen("ibmcloud fn action get ml").read()
+#     namespace = json.loads("".join(data.split("\n")[1:]))["namespace"]
+#     data = os.popen("ibmcloud fn action delete ml").read()
+#     print(data)
+#     data = os.popen("ibmcloud fn action list").read()
+#     if(len(data.split("\n"))<=3):
+#         data = os.popen("ibmcloud fn namespace delete "+namespace).read()
+#         print(data)
+# except:
+#     pass

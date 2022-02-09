1c5cf6723e6f2500e18a13216323306b06e8842b
Nupur Negi <35726589+nupurnegi@users.noreply.github.com>
2022-02-09 13:17:05 +0530

    Update delete_services.py

--- delete_services.py
+++ delete_services.py
@@ -11,8 +11,22 @@ wml_crn = config["PM-20_CRN"]
 ws_crn = config["DATA-SCIENCE-EXPERIENCE_CRN"]
 
 if(updated_cos=="True"):
-    data = os.popen("ibmcloud resource service-instance-delete "+cos_crn+" -f --recursive").read()
-    print(data)
+    status="FAILED"
+    counter=0
+    while(status=="FAILED"):
+        data = os.popen("ibmcloud resource service-instance-delete "+cos_crn+" -f --recursive").read()
+        if (data.find('OK') == -1):        
+            status="FAILED"
+            print("#######Restarting COS Deletion#######")
+            counter=counter+1
+            if(counter==5):
+                status="END"
+        else:
+            status="OK"
+    if(status=="END"):
+        print("##### Sorry we couldn't delete the COS instance. Please go to IBM Cloud Console to Delete the COS instance. #####")
+    else:
+        print(data)
 
 if(updated_wml=="True"):
     data = os.popen("ibmcloud resource service-instance-delete "+wml_crn+" -f --recursive").read()

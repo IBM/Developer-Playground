2b6c413c3ead3848d0794cd0ce9f6735ebb402d9
Nupur Negi <nupurn13@gmail.com>
2022-02-22 01:36:11 +0530

    location fix

--- bankloan/script/deleteservice.sh
+++ bankloan/script/deleteservice.sh
@@ -2,7 +2,7 @@ python3.8 /projects/bank-loan/bankloan/deployment-files/DeleteDeployment.py
 python3.8 /projects/bank-loan/bankloan/deployment-files/delete_services.py
 
 # delete APIKEY using it's id to avoid incorrect APIkey deletion 
-new_filename=$(echo "$a" | sed '2!d' key_file )
+new_filename=$(echo "$a" | sed '2!d' /projects/bank-loan/bankloan/key_file )
 id=$(echo "$a" | cut -c 9-51 <<< $new_filename)
 #delete the API key
 ibmcloud iam api-key-delete $id -f

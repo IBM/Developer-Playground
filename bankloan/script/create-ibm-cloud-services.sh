3b3fb717d2f240ce2ae63b3cbefba45cdcb01b3c
Nupur Negi <35726589+nupurnegi@users.noreply.github.com>
2022-03-14 11:38:51 +0530

    service name dynamic

--- bankloan/script/create-ibm-cloud-services.sh
+++ bankloan/script/create-ibm-cloud-services.sh
@@ -1,16 +1,22 @@
-servicename="cp-cos"
+servicename="idts_cloud-object-storage_"
+servicename+=$CHE_WORKSPACE_ID
+servicename+="_bank-loan"
 service="cloud-object-storage"
 region="global"
 plan="lite"
 python3.8 gather_service_info.py $service $servicename $region $plan || exit 1
 
-servicename="cp-wmachinelearning"
+servicename="idts_pm-20_"
+servicename+=$CHE_WORKSPACE_ID
+servicename+="_bank-loan"
 service="pm-20"
 region="us-south"
 plan="lite"
 python3.8 gather_service_info.py $service $servicename $region $plan || exit 1
 
-servicename="cp-wstudio"
+servicename="idts_data-science-experience_"
+servicename+=$CHE_WORKSPACE_ID
+servicename+="_bank-loan"
 service="data-science-experience"
 region="us-south"
 plan="free-v1"

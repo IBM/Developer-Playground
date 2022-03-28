40a4f107a4cfa45403025b5a130baef649fe33f4
nupurn13@gmail.com <nupurn13@gmail.com>
2022-03-28 09:42:57 +0000

    aws ctas

--- proactive-healthcare-management/aws/create-lambda-producer-function.py
+++ proactive-healthcare-management/aws/create-lambda-producer-function.py
@@ -3,26 +3,30 @@ import sys
 import json
 import dotenv
 from dotenv import dotenv_values
+import zipfile
+
 
 
 config = dotenv_values("../../.env") 
 
 kinesis_stream_name = config["KINESIS_STREAM_NAME"]
 iam_role = config["IAM_ROLE"]
+iam_arn = config["IAM_ARN"]
+string = "const AWS = require('aws-sdk');\nconst KinesisStreamsName = '"+kinesis_stream_name+"';\nconst Region = 'us-east-1'; "
 
-string = "const AWS = require('aws-sdk');\nconst KinesisStreamsName = "+kinesis_stream_name+"';\nconst Region = 'us-east-1'; "
-
-with open("../aws/Producer.js",'r') as contents:
+with open("Producer.js",'r') as contents:
       save = contents.read()
-with open("../aws/Producer.js",'w') as contents:
+with open("Producer.js",'w') as contents:
       contents.write(string)
       contents.write(save)
 
-os.popen("aws s3 cp ../aws/Producer.js s3://cp4dbucket --acl private")
-data = json.loads(os.popen("awscli lambda create-function --function-name Producer --role "+iam_role+" --runtime nodejs14.x --code s3://cp4dbucket/Producer.js").read())
+# os.popen("aws s3 cp Producer.js s3://proactivehealthcarecp4d --acl private")
+with zipfile.ZipFile("Producer.zip", mode="a") as archive:
+    archive.write("Producer.js")
+data = json.loads(os.popen("aws lambda create-function --function-name Producer --role "+iam_arn+" --runtime nodejs14.x --zip-file fileb://Producer.zip --handler Producer.js").read())
 print(data)
 function_arn = data["FunctionArn"]
 dotenv.set_key("../../.env","LAMBDA_PRODUCER_ARN",data["FunctionArn"])
 
-os.popen("sed -i 's,FUNCTION_ARN,'"+function_arn+"',g' event-notif-config.json")
-
+os.popen("sed -i 's,LAMBDA_PRODUCER_ARN,'"+function_arn+"',g' event-notif-config.json")
+os.popen("aws lambda add-permission  --function-name Producer --action lambda:InvokeFunction --statement-id lambda --principal s3.amazonaws.com").read()

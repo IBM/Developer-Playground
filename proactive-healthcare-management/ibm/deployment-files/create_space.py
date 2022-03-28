902562326d386b9313ef051fb363d3a4572fc2eb
nupurn13@gmail.com <nupurn13@gmail.com>
2022-03-28 11:52:51 +0000

    final fix

--- proactive-healthcare-management/ibm/deployment-files/create_space.py
+++ proactive-healthcare-management/ibm/deployment-files/create_space.py
@@ -14,11 +14,11 @@ obj=json.loads(f.read())
 apikey=obj["apikey"]
 
 #add the apikey to .env file
-dotenv.set_key("../../.env","API_KEY",apikey)
+dotenv.set_key("../../../.env","API_KEY",apikey)
 
-dotenv.set_key("../../.env","DEPLOYMENT_SPACE_NAME",deployment_space_name)
+dotenv.set_key("../../../.env","DEPLOYMENT_SPACE_NAME",deployment_space_name)
 
-config = dotenv_values("../../.env") 
+config = dotenv_values("../../../.env") 
 model_name = config["MODEL_NAME"]
 cos_crn = config["CLOUD-OBJECT-STORAGE_CRN"]
 wml_crn = config["PM-20_CRN"]
@@ -49,7 +49,7 @@ spaces_details = client.spaces.store(meta_props=metadata,background_mode=False)
 space_id = client.spaces.get_id(spaces_details)
 
 
-dotenv.set_key("../../.env","SPACE_ID",space_id)
+dotenv.set_key("../../../.env","SPACE_ID",space_id)
 
 client.set.default_space(space_id)
 i=0
@@ -64,15 +64,16 @@ while details["resources"][0]["entity"]["status"]["state"] != "completed" and de
     details = client.import_assets.get_details(space_id=space_id)
 
 asset_details = client.repository.get_details()
+print(asset_details)
 for resource in asset_details["models"]["resources"] :
     if(resource["metadata"]["name"] == model_name):
-        dotenv.set_key("../../.env","MODEL_ID",resource["metadata"]["id"])
+        dotenv.set_key("../../../.env","MODEL_ID",resource["metadata"]["id"])
         print("Deployment Space with ID " + space_id + " and name "+ deployment_space_name +" containing model " + model_name + " is created successfully.")
     else:
         print("Model Import Failed!!!.")
 
 
-config = dotenv_values("../../.env") 
+config = dotenv_values("../../../.env") 
 try:
     published_model_id=config["MODEL_ID"]
 except:

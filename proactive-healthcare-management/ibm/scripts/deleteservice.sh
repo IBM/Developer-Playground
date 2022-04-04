b0bfef0f2d41f31eeca753ac866cd9ea85f59e11
Nupur Negi <35726589+nupurnegi@users.noreply.github.com>
2022-04-04 12:14:56 +0530

    Update deleteservice.sh

--- proactive-healthcare-management/ibm/scripts/deleteservice.sh
+++ proactive-healthcare-management/ibm/scripts/deleteservice.sh
@@ -1,5 +1,5 @@
 python3.8 ${CHE_PROJECTS_ROOT}/aws-healthcare/proactive-healthcare-management/ibm/deployment-files/DeleteDeployment.py
-python3.8 ${CHE_PROJECTS_ROOT}/aws-healthcare/proactive-healthcare-management/ibm/delete_services.py
+python3.8 ${CHE_PROJECTS_ROOT}/aws-healthcare/proactive-healthcare-management/ibm/scripts/delete_services.py
 
 # delete APIKEY using it's id to avoid incorrect APIkey deletion 
 new_filename=$(echo "$a" | sed '2!d' ${CHE_PROJECTS_ROOT}/aws-healthcare/proactive-healthcare-management/ibm/key_file )

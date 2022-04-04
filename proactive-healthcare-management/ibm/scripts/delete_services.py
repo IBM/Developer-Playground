e28d62697cefabac1a86144d69ecda431650d855
Nupur Negi <35726589+nupurnegi@users.noreply.github.com>
2022-04-04 10:09:32 +0530

    Create delete_services.py

---
+++ proactive-healthcare-management/ibm/scripts/delete_services.py
@@ -0,0 +1,8 @@
+python3.8 ${CHE_PROJECTS_ROOT}/aws-healthcare/proactive-healthcare-management/ibm/delete_services.py
+
+# delete APIKEY using it's id to avoid incorrect APIkey deletion 
+new_filename=$(echo "$a" | sed '2!d' ${CHE_PROJECTS_ROOT}/aws-healthcare/proactive-healthcare-management/ibm/key_file )
+id=$(echo "$a" | cut -c 9-51 <<< $new_filename)
+#delete the API key
+ibmcloud iam api-key-delete $id -f
+rm ${CHE_PROJECTS_ROOT}/aws-healthcare/proactive-healthcare-management/ibm/key_file

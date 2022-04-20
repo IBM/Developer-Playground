c0f7e756f4369d9a82dac3bcb584b7970408deee
Rajesh Boda <rajeshkumarboda@in.ibm.com>
2022-04-20 16:02:28 +0530

    Adding techzone assets to staging

---
+++ didact/demo-builder.js
@@ -0,0 +1,34 @@
+window.onload = function funLoad() { 
+    document.getElementById('createusersteps').style.display ='none';
+    document.getElementById('updateusersteps').style.display = 'none';
+    document.querySelector("#createusersoption").addEventListener('click',show1);
+    document.querySelector("#updateusersoption").addEventListener('click',show2);
+
+    function show1(){
+      document.getElementById('createusersteps').style.display ='inherit';
+      document.getElementById('updateusersteps').style.display = 'none';
+    
+    }
+    function show2(){
+      document.getElementById('createusersteps').style.display ='none';
+      document.getElementById('updateusersteps').style.display = 'inherit';
+    }
+
+    var governanceOptions = document.getElementsByName("governanceartifactsopt");
+    for( let key in governanceOptions) {
+      document.getElementById(governanceOptions[key].value).style.display ='none';
+      document.getElementById(governanceOptions[key].id).addEventListener('click',handleGovernanceOptions);
+      if(governanceOptions[key].checked ) {
+        document.getElementById(governanceOptions[key].value).style.display ='inherit';
+      }
+    }
+    
+    function handleGovernanceOptions(){
+      for( let key in governanceOptions) {
+        document.getElementById(governanceOptions[key].value).style.display ='none';
+        if(governanceOptions[key].checked ) {
+          document.getElementById(governanceOptions[key].value).style.display ='inherit';
+        }
+      }
+    }
+} 

92e6c96b2a0f5b4d50e350583270188f0df6753a
Amol Deshpande <>
2022-04-28 11:32:16 +0530

    drop down ui changes

--- didact/olm-utils.js
+++ didact/olm-utils.js
@@ -14,9 +14,10 @@ window.onload = function () {
 
   function install_olm() {
     let component_list = document.getElementById("olm_component_list").value;
-    let storage_vendor = document.getElementById("storage_vendor").value;
     let release_version = document.getElementById("olm_release_version").value;
-    let preview_value = document.getElementById("olm_preview_value").value;
+    // let preview_value = document.getElementById("olm_preview_value").value;
+    let select = document.getElementById('olm_preview_value');
+    let preview_value = select.options[select.selectedIndex].text;
     document.getElementById("command_exec").href =
       "didact://?commandId=vscode.didact.sendNamedTerminalAString&&text=sandbox terminal$$run_utils apply-olm --preview=" +
       preview_value +
@@ -32,8 +33,10 @@ window.onload = function () {
   function install_cr() {
     let component_list = document.getElementById("cr_component_list").value;
     let release_version = document.getElementById("cr_release_version").value;
-    let preview_value = document.getElementById("cr_preview_value").value;
-    let license_acceptance = document.getElementById("cr_license_acceptance").value;
+    let select = document.getElementById('cr_preview_value');
+    let preview_value = select.options[select.selectedIndex].text;
+    select = document.getElementById('cr_license_acceptance');
+    let license_acceptance = select.options[select.selectedIndex].text;
     let cr_cpd_instance = document.getElementById("cr_cpd_instance").value;
     let cr_storage_class = document.getElementById("cr_storage_class").value;
     let cr_storage_vendor=document.getElementById("cr_storage_vendor").value;

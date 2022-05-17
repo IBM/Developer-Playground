4210d65acc90098120c2013feeb06eaebdd2a84c
Nupur Negi <35726589+nupurnegi@users.noreply.github.com>
2022-05-17 23:42:05 +0530

    Update gather_service_info.py

--- proactive-healthcare-management/ibm/scripts/gather_service_info.py
+++ proactive-healthcare-management/ibm/scripts/gather_service_info.py
@@ -13,7 +13,7 @@ print(data)
 if(len(data.split("\n"))<3):
     data = json.loads(os.popen("ibmcloud resource service-instances --service-name "+service+" --output json").read())
     print(os.popen("ibmcloud resource service-instances --service-name "+service).read())
-    result = [data[0]["region_id"],data[0]["name"].replace(" ", "%20")]
+    result = [data[0]["region_id"],data[0]["name"]]
     choice = 0
     while (choice!="y" and choice!="Y" and choice!="n" and choice!="N") :
         print("Creating "+service+" Service Failed as you already have an instance.")
@@ -24,17 +24,17 @@ if(len(data.split("\n"))<3):
                 raise Exception("Service creation stopped.")
             else:
                 choice=0
-    dotenv.set_key("../../../.env",service.upper()+"_NAME",result[1])
-    dotenv.set_key("../../../.env",service.upper()+"_LOC",result[0])
-    data = os.popen("ibmcloud resource service-instance "+result[1]+" --id").read()
-    dotenv.set_key("../../../.env",service.upper()+"_CRN",data.split("\n")[-2].split(" ")[0])
-    dotenv.set_key("../../../.env",service.upper()+"_UPDATED","False")
+    dotenv.set_key("../.env",service.upper()+"_NAME",result[1])
+    dotenv.set_key("../.env",service.upper()+"_LOC",result[0])
+    data = os.popen("ibmcloud resource service-instance '"+result[1]+"' --id").read()
+    dotenv.set_key("../.env",service.upper()+"_CRN",data.split("\n")[-2].split(" ")[0])
+    dotenv.set_key("../.env",service.upper()+"_UPDATED","False")
 else:
-    dotenv.set_key("../../../.env",service.upper()+"_NAME",servicename)
-    dotenv.set_key("../../../.env",service.upper()+"_LOC",region)
-    data = os.popen("ibmcloud resource service-instance "+servicename+" --id").read()
-    dotenv.set_key("../../../.env",service.upper()+"_CRN",data.split("\n")[-2].split(" ")[0])
-    dotenv.set_key("../../../.env",service.upper()+"_UPDATED","True")
+    dotenv.set_key("../.env",service.upper()+"_NAME",servicename)
+    dotenv.set_key("../.env",service.upper()+"_LOC",region)
+    data = os.popen("ibmcloud resource service-instance '"+servicename+"' --id").read()
+    dotenv.set_key("../.env",service.upper()+"_CRN",data.split("\n")[-2].split(" ")[0])
+    dotenv.set_key("../.env",service.upper()+"_UPDATED","True")
 print("###########################################################")        
-os.system("echo Updated "+service+" instance details successfuly")
-print("###########################################################")         
+os.system("echo Updated "+service+" instance details successfully")
+print("###########################################################")

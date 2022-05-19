add075cff5699aa6df571addff3afae6d58092f6
Suyash Gupte <Suyash.Gupte2@ibm.com>
2022-05-19 18:23:28 +0530

    Update import-demo-artifacts.js

--- didact/import-demo-artifacts.js
+++ didact/import-demo-artifacts.js
@@ -1,5 +1,5 @@
 window.onload = function funLoad() {
-  let compositeHref = "didact://?commandId=extension.compositeCommand&&text=terminal-for-sandbox-container:new%7Cvscode.didact.sendNamedTerminalAString%2Csandbox%20terminal%2Cgit%20clone%20-b%20techzone%20https%3A%2F%2Fgithub.com%2FIBM%2FDeveloper-Playground%20%24%7BCHE_PROJECTS_ROOT%7D%2Ftechzone-demo%2C%2Fprojects%7Cvscode.didact.sendNamedTerminalAString%2Cgit%20terminal%2Csh%20/projects/techzone-demo/sandbox/getDemoFiles.sh%20demo_name%7Cvscode.didact.sendNamedTerminalAString%2Csandbox%20terminal%2Ccd%20${CHE_PROJECTS_ROOT}/techzone-demo;pip3.8%20install%20-r%20requirements.txt%3Bcd%20%2Fprojects%2Ftechzone-demo%2Fsandbox%2F"
+  let compositeHref = "didact://?commandId=extension.compositeCommand&&text=terminal-for-sandbox-container:new%7Cvscode.didact.sendNamedTerminalAString%2Csandbox%20terminal%2Cgit%20clone%20-b%20techzone%20https%3A%2F%2Fgithub.com%2FIBM%2FDeveloper-Playground%20%24%7BCHE_PROJECTS_ROOT%7D%2Ftechzone-demo%2C%2Fprojects%7Cvscode.didact.sendNamedTerminalAString%2Cgit%20terminal%2Csh%20/projects/techzone-demo/sandbox/getDemoFiles.sh%20demo_name%7Cvscode.didact.sendNamedTerminalAString%2Csandbox%20terminal%2Ccd%20${CHE_PROJECTS_ROOT}/techzone-demo;pip3.8%20install%20-r%20requirements.txt%3Bcd%20%2Fprojects%2Ftechzone-demo%2Fsandbox%2F%3Bpython3.8%20update-env.py%20"
   let prerequisite = ["hostname", "wkcuser", "password"]
   let didact = document.getElementsByClassName("apptitle")[0].textContent
 
@@ -10,6 +10,7 @@ window.onload = function funLoad() {
     hostname: "",
     wkcuser: "",
     password: "",
+    demo:""
   }
 
   //Create localStorage item if didact name not present 
@@ -24,6 +25,9 @@ window.onload = function funLoad() {
 
   //Fill input data from localStorage
   prerequisite.forEach(input => document.getElementsByName(input)[0].value = JSON.parse(localStorage[didact])[input])
+  
+
+  //Disable timeline
   let localData = JSON.parse(localStorage[didact])
   let timelineContainer = document.getElementsByClassName("timeline-container")[0]
   if (localData.hostname.trim() === "" || localData.wkcuser.trim() === "" || localData.password.trim() === "") {
@@ -32,14 +36,22 @@ window.onload = function funLoad() {
     [...timelineContainer.getElementsByTagName("A")].forEach(ele => ele.style.pointerEvents = "none");
     [...timelineContainer.getElementsByTagName("INPUT")].forEach(ele => ele.style.pointerEvents = "none")
   }
+
+  //Store Config data
   let config = {
     hostname: localData.hostname,
     wkcuser: localData.wkcuser,
     password: localData.password,
   }
+  let demo = localData.demo
+
   let cta = document.getElementById("configure-env")
-  compositeHref=`${compositeHref}${Object.values(config).toString().replaceAll(",", "%20")}`
-  cta.href = compositeHref
+  cta.href =`${compositeHref.replace("demo_name",demo)}${Object.values(config).toString().replaceAll(",", "%20")}`
+
+  if(demo.trim() !== ""){
+    document.getElementById("selected").textContent = demo
+    document.getElementById("import-project").href = `didact://?commandId=vscode.didact.sendNamedTerminalAString&&text=sandbox terminal$$cd /projects/techzone-demo/sandbox/;python3.8 importProject.py project_assets ${demo}`
+  }
   
   let checkList = document.getElementById('list1');
   document.onclick = function (e) {
@@ -59,7 +71,8 @@ window.onload = function funLoad() {
   [...options].forEach(option=> option.addEventListener("click",selectOption))
   function selectOption(e){
     document.getElementById("selected").textContent = e.target.textContent
-    cta.href=compositeHref.replace("demo_name",e.target.textContent)
+    demo = e.target.textContent
+    cta.href=`${compositeHref.replace("demo_name",e.target.textContent)}${Object.values(config).toString().replaceAll(",", "%20")}`
     document.getElementById("import-project").href = `didact://?commandId=vscode.didact.sendNamedTerminalAString&&text=sandbox terminal$$cd /projects/techzone-demo/sandbox/;python3.8 importProject.py project_assets ${e.target.textContent}`
     checkList.classList.remove('visible');
   }
@@ -76,8 +89,10 @@ window.onload = function funLoad() {
     }
     config[e.target.name] = e.target.value
     let cta = document.getElementById("configure-env")
-    
-    cta.href = `${compositeHref}${Object.values(config).toString().replaceAll(",", "%20")}%7Cpython3%20update-project-dropdown.py%7Cvscode.didact.reload`
+    cta.href = `${compositeHref.replace("demo_name",demo)}${Object.values(config).toString().replaceAll(",", "%20")}`
+    let tempData = JSON.parse(localStorage[didact])
+    tempData[e.target.name] = e.target.value
+    localStorage[didact] = JSON.stringify(tempData)
     valid = true
     for (val of Object.values(config)) {
       if (val.trim() === "")

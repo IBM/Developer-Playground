746526064ee178645783b77d8dbcff1d55df0b34
Amol Deshpande <>
2022-06-02 16:24:55 +0530

    Update github.sh

--- sandbox/github.sh
+++ sandbox/github.sh
@@ -3,8 +3,13 @@ demo_name=$1
 meta_data=$2
 sandbox_username=$3
 desc=$4
-
-# TODO GIT Creds logic
+branch="main"
+if [[ $PLAYGROUND_ENVIRONMENT == *"development"* ]]; then
+      branch="development" 
+fi
+if [[ $PLAYGROUND_ENVIRONMENT == *"staging"* ]]; then
+      branch="staging" 
+fi
 github_url=https://github.com/IBM/ibmtechzone-demo-artifacts
 project_name=ibmtechzone-demo-artifacts 
 username=IBM
@@ -13,14 +18,12 @@ git_token=Z2hwX0lReDhlZHFnS3BpNVBYTXdYTWhRNk01d0diUHFzRjQxdGIzdQ==
 cd /projects/techzone-demo/sandbox
 rm -rf $project_name
 if [ ! -d "$project_name" ]; then
-  git clone --sparse $github_url
+  git clone --single-branch --branch $branch --sparse  $github_url 
 fi
 # TODO
 # Update the actual url
 cd /projects/techzone-demo/sandbox/$project_name
-
 git checkout -b $sandbox_username-$demo_name
-git remote add upstream $github_url
 git sparse-checkout set $sandbox_username-$demo_name
 if [ -d "$sandbox_username-$demo_name" ]; then
   # Control will enter here if $DIRECTORY doesn't exist.
@@ -29,16 +32,18 @@ if [ -d "$sandbox_username-$demo_name" ]; then
 fi
 gtd=`echo $git_token | base64 -d`
 mkdir $sandbox_username-$demo_name
-git sparse-checkout set $sandbox_username-$demo_name
+git sparse-checkout set $sandbox_username-$demo_name 
 # TODO
 # Add the meta_data and files to demo_name variable
 # ......
 echo $meta_data > $sandbox_username-$demo_name/readme.json
-# add the required files
+# # add the required files
 cp /projects/techzone-demo/sandbox/governance_artifacts.zip $sandbox_username-$demo_name/
 cp /projects/techzone-demo/sandbox/users.csv $sandbox_username-$demo_name/
 cp /projects/techzone-demo/sandbox/data_protection_rules.json $sandbox_username-$demo_name/
 cp /projects/techzone-demo/sandbox/project_assets.zip $sandbox_username-$demo_name/
+# Now
+cp ../demo_users.csv $sandbox_username-$demo_name/
 
 
 git add .
@@ -49,8 +54,9 @@ curl \
   -X POST \
   -H 'Authorization: token '${gtd}'' \
   https://api.github.com/repos/${username}/${project_name}/pulls \
-  -d '{"title":"Sandbox Demo by '$sandbox_username'","body":"","head":"'$sandbox_username-$demo_name'","base":"main"}'
+  -d '{"title":"Sandbox Demo by '$sandbox_username'","body":"","head":"'$sandbox_username-$demo_name'","base":"'$branch'"}'
 
 cd /projects/techzone-demo/sandbox
 rm -rf $project_name
 echo "Pull Request created successfully!"   
+

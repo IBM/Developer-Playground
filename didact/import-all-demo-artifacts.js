d5edaa6782a218ca4c459f81b57e2631b1954f61
Suyash Gupte <Suyash.Gupte2@ibm.com>
2022-06-10 11:14:13 +0530

    update didacts

--- didact/import-all-demo-artifacts.js
+++ didact/import-all-demo-artifacts.js
@@ -1,11 +1,7 @@
 window.onload = function funLoad() {
-
   let compositeHref = "didact://?commandId=extension.compositeCommand&&text=terminal-for-sandbox-container:new%7Cvscode.didact.sendNamedTerminalAString%2Csandbox%20terminal%2Cgit%20clone%20-b%20techzone%20https%3A%2F%2Fgithub.com%2FIBM%2FDeveloper-Playground%20%24%7BCHE_PROJECTS_ROOT%7D%2Ftechzone-demo%2C%2Fprojects%7Cvscode.didact.sendNamedTerminalAString%2Csandbox%20terminal%2Cbash%20/projects/techzone-demo/sandbox/getDemoFiles.sh%20demo_name%7Cvscode.didact.sendNamedTerminalAString%2Csandbox%20terminal%2Ccd%20${CHE_PROJECTS_ROOT}/techzone-demo;pip3.8%20install%20-r%20requirements.txt%3Bcd%20%2Fprojects%2Ftechzone-demo%2Fsandbox%2F%3Bpython3.8%20update-env.py%20"
   let prerequisite = ["hostname", "wkcuser", "password"]
-
-  let demo = document.getElementById("selected-demo").textContent
-
-  let didact = demo ? `${document.getElementsByClassName("apptitle")[0].textContent}-${demo}` : document.getElementsByClassName("apptitle")[0].textContent
+  let didact = document.getElementsByClassName("apptitle")[0].textContent
 
   //Get Workspace ID and setup default data for localStorage
   let workspaceId = document.getElementById("workspaceID").textContent
@@ -40,6 +36,10 @@ window.onload = function funLoad() {
     [...timelineContainer.getElementsByTagName("A")].forEach(ele => ele.style.pointerEvents = "none");
     [...timelineContainer.getElementsByTagName("INPUT")].forEach(ele => ele.style.pointerEvents = "none");
     [...timelineContainer.getElementsByTagName("DETAILS")].forEach(ele => ele.style.pointerEvents = "none");
+    let checkList = document.getElementById('list1');
+    checkList.style.pointerEvents = "none";
+
+
   }
 
   //Store Config data
@@ -48,17 +48,45 @@ window.onload = function funLoad() {
     wkcuser: localData.wkcuser,
     password: localData.password,
   }
-
-
-  //username from demo
-  document.getElementById("selected-demo").textContent = demo.split(/-(.*)/s)[1] ? demo.split(/-(.*)/s)[1] : demo
+  let demo = localData.demo
 
   //modify cta with localStorage data
   let cta = document.getElementById("configure-env")
   cta.href = `${compositeHref.replace("demo_name", demo)}${Object.values(config).toString().replaceAll(",", "%20")}`
-  document.getElementById("import-project").href = `didact://?commandId=vscode.didact.sendNamedTerminalAString&&text=sandbox terminal$$cd /projects/techzone-demo/sandbox/;python3.8 importProject.py project_assets ${demo}`
+
+  if (demo.trim() !== "") {
+    document.getElementById("selected").textContent = demo
+    document.getElementById("import-project").href = `didact://?commandId=vscode.didact.sendNamedTerminalAString&&text=sandbox terminal$$cd /projects/techzone-demo/sandbox/;python3.8 importProject.py project_assets ${demo}`
+  }
 
 
+  //open/close demo dropdown
+  let checkList = document.getElementById('list1');
+  document.onclick = function (e) {
+    if (e.target.parentElement !== checkList && e.target.name !== "governance-artifacts" && e.target.nodeName !== "LI") {
+      checkList.classList.remove('visible');
+    }
+  };
+  checkList.getElementsByClassName('anchor')[0].onclick = function (evt) {
+    if (checkList.classList.contains('visible'))
+      checkList.classList.remove('visible');
+    else
+      checkList.classList.add('visible');
+  }
+
+  //modify cta with selected demo value
+  let options = checkList.getElementsByTagName('LI');
+  [...options].forEach(option => option.addEventListener("click", selectOption))
+  function selectOption(e) {
+    document.getElementById("selected").textContent = e.target.textContent
+    demo = e.target.textContent
+    cta.href = `${compositeHref.replace("demo_name", e.target.textContent)}${Object.values(config).toString().replaceAll(",", "%20")}`
+    document.getElementById("import-project").href = `didact://?commandId=vscode.didact.sendNamedTerminalAString&&text=sandbox terminal$$cd /projects/techzone-demo/sandbox/;python3.8 importProject.py project_assets ${e.target.textContent}`
+    checkList.classList.remove('visible');
+    let tempData = JSON.parse(localStorage[didact])
+    tempData["demo"] = e.target.textContent
+    localStorage[didact] = JSON.stringify(tempData)
+  }
 
   //get env values
   let envVariables = document.getElementsByClassName('env-variables');
@@ -87,12 +115,16 @@ window.onload = function funLoad() {
       [...timelineContainer.getElementsByTagName("A")].forEach(ele => ele.style.pointerEvents = "auto");
       [...timelineContainer.getElementsByTagName("INPUT")].forEach(ele => ele.style.pointerEvents = "auto");
       [...timelineContainer.getElementsByTagName("DETAILS")].forEach(ele => ele.style.pointerEvents = "auto");
+      let checkList = document.getElementById('list1');
+      checkList.style.pointerEvents = "auto";
     } else {
       timelineContainer.style.opacity = 0.5;
       timelineContainer.style.cursor = "not-allowed";
       [...timelineContainer.getElementsByTagName("A")].forEach(ele => ele.style.pointerEvents = "none");
       [...timelineContainer.getElementsByTagName("INPUT")].forEach(ele => ele.style.pointerEvents = "none");
       [...timelineContainer.getElementsByTagName("DETAILS")].forEach(ele => ele.style.pointerEvents = "none");
+      let checkList = document.getElementById('list1');
+      checkList.style.pointerEvents = "none";
     }
   }
 }

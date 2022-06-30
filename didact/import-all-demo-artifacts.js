fe6d977bebf4a29d613ff7f850023ca1eeecbd50
Suyash Gupte <Suyash.Gupte2@ibm.com>
2022-06-30 19:05:31 +0530

    feedback changes

--- didact/import-all-demo-artifacts.js
+++ didact/import-all-demo-artifacts.js
@@ -1,5 +1,8 @@
 window.onload = function funLoad() {
-  let compositeHref = "didact://?commandId=extension.compositeCommand&&text=terminal-for-sandbox-container:new%7Cvscode.didact.sendNamedTerminalAString%2Csandbox%20terminal%2Cgit%20clone%20-b%20techzone%20https%3A%2F%2Fgithub.com%2FIBM%2FDeveloper-Playground%20%24%7BCHE_PROJECTS_ROOT%7D%2Ftechzone-demo%2C%2Fprojects%7Cvscode.didact.sendNamedTerminalAString%2Csandbox%20terminal%2Cbash%20/projects/techzone-demo/sandbox/getDemoFiles.sh%20demo_name%7Cvscode.didact.sendNamedTerminalAString%2Csandbox%20terminal%2Ccd%20${CHE_PROJECTS_ROOT}/techzone-demo;pip3.8%20install%20-r%20requirements.txt%3Bcd%20%2Fprojects%2Ftechzone-demo%2Fsandbox%2F%3Bpython3.8%20update-env.py%20"
+  let env = document.getElementById("environment").textContent
+  let compositeHref = "didact://?commandId=extension.compositeCommand&&text=terminal-for-sandbox-container:new%7Cvscode.didact.sendNamedTerminalAString%2Csandbox%20terminal%2Cgit%20clone%20-b%20$BRANCH%20https%3A%2F%2Fgithub.com%2FIBM%2FCPDemoFramework%20%24%7BCHE_PROJECTS_ROOT%7D%2Ftechzone-demo%2C%2Fprojects%7Cvscode.didact.sendNamedTerminalAString%2Csandbox%20terminal%2Cbash%20/projects/techzone-demo/sandbox/getDemoFiles.sh%20demo_name%7Cvscode.didact.sendNamedTerminalAString%2Csandbox%20terminal%2Ccd%20${CHE_PROJECTS_ROOT}/techzone-demo;pip3.8%20install%20-r%20requirements.txt%3Bcd%20%2Fprojects%2Ftechzone-demo%2Fsandbox%2F%3Bpython3.8%20update-env.py%20"
+  compositeHref = compositeHref.replaceAll("$BRANCH",env)
+  
   let prerequisite = ["hostname", "wkcuser", "password"]
   let didact = document.getElementsByClassName("apptitle")[0].textContent
 
@@ -49,6 +52,11 @@ window.onload = function funLoad() {
     password: localData.password,
   }
   let demo = localData.demo
+
+  //update open cluster cta URL
+  let clusterUrl = `https://${config.hostname}`
+  let openClusterCta = document.getElementById("open-cpd-cluster")
+  openClusterCta.href = clusterUrl
 
   //modify cta with localStorage data
   let cta = document.getElementById("configure-env")
@@ -126,5 +134,8 @@ window.onload = function funLoad() {
       let checkList = document.getElementById('list1');
       checkList.style.pointerEvents = "none";
     }
+    let clusterUrl = `https://${config.hostname}`
+    let openClusterCta = document.getElementById("open-cpd-cluster")
+    openClusterCta.href = clusterUrl
   }
 }

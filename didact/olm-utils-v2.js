cd85c1e2dc3b5c8087c8ff57b037d78191d168a9
Nupur Negi <35726589+nupurnegi@users.noreply.github.com>
2022-08-08 15:26:26 +0530

    Update olm-utils-v2.js

--- didact/olm-utils-v2.js
+++ didact/olm-utils-v2.js
@@ -152,12 +152,13 @@ window.onload = function () {
   // }
 
   document.getElementById("install_cr").addEventListener("click", install_cr);
-
+  
   function install_cr() {
+    let cp4dVersion = document.getElementById('cp4d_version').value;
     let component_list = selectedServices.toString()
     let storage = document.getElementById("cr_storage_value").value;
     document.getElementById("command_exec").href =
-      "didact://?commandId=vscode.didact.sendNamedTerminalAString&&text=sandbox%20terminal$$cd%20${CHE_PROJECTS_ROOT}/techzone/olm-utils-v2/;pip3.8%20install%20PyYAML;python3.8%20updateYaml.py%20"+component_list+"%20"+storage+"; source env.sh";
+      "didact://?commandId=vscode.didact.sendNamedTerminalAString&&text=sandbox%20terminal$$cd%20${CHE_PROJECTS_ROOT}/techzone/olm-utils-v2/;pip3.8%20install%20PyYAML;python3.8%20updateYaml.py%20"+component_list+"%20"+storage+"%20"+cp4dVersion+"; source env.sh";
     document.getElementById("command_exec").click();
   }
   // Preview logic

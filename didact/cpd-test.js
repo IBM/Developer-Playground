2a443f8ca6dcf85fdcae6a45b837bc0cb16c996e
Amol Deshpande <>
2022-08-23 16:52:16 +0530

    Update cpd-test.js

--- didact/cpd-test.js
+++ didact/cpd-test.js
@@ -121,7 +121,7 @@ window.onload = function () {
         //configure cta
         document.getElementById("configure-env").addEventListener("click", updateConfigVars);
         function updateConfigVars(e){
-          document.getElementById("config_command_exec").href =`${compositeHref}${Object.keys(config).map(val => `${document.getElementsByName(val)[0].value}`).toString().replaceAll(",","%20")}`
+          document.getElementById("config_command_exec").href =`${compositeHref}${Object.keys(config).map(val => `${document.getElementsByName(val)[0].value}||\"\"`).toString().replaceAll(",","%20")}`
           document.getElementById("config_command_exec").click();
         }
 

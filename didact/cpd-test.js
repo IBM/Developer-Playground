70ba7b768da947a568b60d98a43198731a06e275
amol-m-deshpande <amol.deshpande@in.ibm.com>
2022-08-29 12:06:38 +0530

    Update cpd-test.js

--- didact/cpd-test.js
+++ didact/cpd-test.js
@@ -136,7 +136,7 @@ window.onload = function () {
       }
       let storage = document.getElementById("cr_storage_value").value;
       document.getElementById("command_exec").href =
-        "didact://?commandId=vscode.didact.sendNamedTerminalAString&&text=sandbox%20terminal$$cd%20${CHE_PROJECTS_ROOT}/techzone/olm-utils-v2/;pip3.8%20install%20PyYAML;python3.8%20updateYaml.py%20"+component_list+"%20"+storage+"%20"+cp4dVersion+"; source deploy.sh";
+        "didact://?commandId=vscode.didact.sendNamedTerminalAString&&text=sandbox%20terminal$$cd%20${CHE_PROJECTS_ROOT}/techzone/olm-utils-v2/;pip3.8%20install%20PyYAML;python3.8%20updateYaml.py%20"+component_list+"%20"+storage+"%20"+cp4dVersion+";bash deploy.sh";
       document.getElementById("command_exec").click();
     }
 

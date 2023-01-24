df3073d40a1c57881d069ca81010e63ed6001089
Nupur Negi <35726589+nupurnegi@users.noreply.github.com>
2023-01-24 10:07:42 +0530

    Update app.py

--- video-insights/app.py
+++ video-insights/app.py
@@ -21,4 +21,4 @@ if __name__ == '__main__':
     from apis import *
     app.secret_key = os.urandom(12)
     app.run(host='0.0.0.0', port=port, debug=True)
-    print("success)
+    print("success")
